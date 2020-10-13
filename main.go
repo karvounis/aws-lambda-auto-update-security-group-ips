89ecea87400389f7ef9a77e54254c11bcc7dbd71
Vaggelis Karvounis <ekarvounis@protonmail.com>
2020-10-13 23:31:23 +0300

    describe instances 1 by 1

--- main.go
+++ main.go
@@ -35,61 +35,38 @@ func Handler(request Request) (response string, err error) {
 	ec2Svc := ec2.New(sess)
 	autoscalingSvc := autoscaling.New(sess)
 
-	asgIPs, err := getASGPublicIPs(request, asgName, logger, autoscalingSvc, ec2Svc)
+	asgIPs, err := getASGPublicIPs(request, asgName, autoscalingSvc, ec2Svc)
 	if err != nil {
 		logger.Error("Failed to get ASG Public IPs", zap.Error(err))
 		return response, err
 	}
 	logger.Info("AutoScaling Group's IPs", zap.Any("asgIPs", asgIPs))
 
-	sgResponse, err := ec2Svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
-		GroupIds: []*string{
-			aws.String(sgID),
-		},
-	})
+	sgIPs, err := getSGIPs(sgID, ec2Svc)
 	if err != nil {
-		logger.Error("Failed to describe security groups", zap.Error(err))
+		logger.Error("Failed to get the IPs of the Security Groups", zap.Error(err))
 		return response, err
-	}
-
-	sgIPs := make(map[string]string)
-	for _, ipRange := range sgResponse.SecurityGroups[0].IpPermissions[0].IpRanges {
-		sgIPs[aws.StringValue(ipRange.CidrIp)] = aws.StringValue(ipRange.CidrIp)
 	}
 	logger.Info("Security Group's IPs", zap.Any("sgIPs", sgIPs))
 
-	var ipsToAdd []string
-	for i, _ := range asgIPs {
-		if _, ok := sgIPs[i]; !ok {
-			ipsToAdd = append(ipsToAdd, i)
-		}
-	}
+	ipsToAdd := getIPsToAdd(asgIPs, sgIPs)
 	logger.Info("IPs to add", zap.Any("ipsToAdd", ipsToAdd))
 
-	var ipsToRemove []string
-	for i, _ := range sgIPs {
-		if _, ok := asgIPs[i]; !ok {
-			ipsToRemove = append(ipsToRemove, i)
-		}
-	}
+	ipsToRemove := getIPsToRemove(asgIPs, sgIPs)
 	logger.Info("IPs to remove", zap.Any("ipsToRemove", ipsToRemove))
 
 	if len(ipsToAdd) != 0 {
 		var addParams []*ec2.IpPermission
-		for _, v := range ipsToAdd {
+		for _, ip := range ipsToAdd {
 			addParams = append(addParams, &ec2.IpPermission{
-				FromPort: aws.Int64(0),
-				ToPort:   aws.Int64(65535),
-				IpRanges: []*ec2.IpRange{
-					{
-						CidrIp: aws.String(v),
-					},
-				},
+				FromPort:   aws.Int64(0),
+				ToPort:     aws.Int64(65535),
+				IpRanges:   []*ec2.IpRange{{CidrIp: aws.String(ip)}},
 				IpProtocol: aws.String("tcp"),
 			})
 		}
 		logger.Info("IPs to add request", zap.Any("addParams", addParams))
-		res2, err := ec2Svc.AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
+		_, err := ec2Svc.AuthorizeSecurityGroupIngress(&ec2.AuthorizeSecurityGroupIngressInput{
 			GroupId:       aws.String(sgID),
 			IpPermissions: addParams,
 		})
@@ -97,24 +74,19 @@ func Handler(request Request) (response string, err error) {
 			logger.Error("Failed to add IPs to security group", zap.Error(err))
 			return response, err
 		}
-		logger.Info("AuthorizeSecurityGroupIngress", zap.Any("res2", res2))
 	}
 
 	if len(ipsToRemove) != 0 {
 		var removeParams []*ec2.IpPermission
 		for _, v := range ipsToRemove {
 			removeParams = append(removeParams, &ec2.IpPermission{
-				FromPort: aws.Int64(0),
-				ToPort:   aws.Int64(65535),
-				IpRanges: []*ec2.IpRange{
-					{
-						CidrIp: aws.String(v),
-					},
-				},
+				FromPort:   aws.Int64(0),
+				ToPort:     aws.Int64(65535),
+				IpRanges:   []*ec2.IpRange{{CidrIp: aws.String(v)}},
 				IpProtocol: aws.String("tcp"),
 			})
 		}
-		res3, err := ec2Svc.RevokeSecurityGroupIngress(&ec2.RevokeSecurityGroupIngressInput{
+		_, err := ec2Svc.RevokeSecurityGroupIngress(&ec2.RevokeSecurityGroupIngressInput{
 			GroupId:       aws.String(sgID),
 			IpPermissions: removeParams,
 		})
@@ -122,48 +94,76 @@ func Handler(request Request) (response string, err error) {
 			logger.Error("Failed to remove IPs from security group", zap.Error(err))
 			return response, err
 		}
-		logger.Info("RevokeSecurityGroupIngress", zap.Any("res3", res3))
 	}
 
 	return "All ok!", err
 }
 
-func getASGPublicIPs(event Request, asgName string, logger *zap.Logger, autoscalingSvc *autoscaling.AutoScaling, ec2Svc *ec2.EC2) (map[string]string, error) {
+func getIPsToAdd(asgIPs map[string]string, sgIPs map[string]string) (ipsToAdd []string) {
+	for i, _ := range asgIPs {
+		if _, ok := sgIPs[i]; !ok {
+			ipsToAdd = append(ipsToAdd, i)
+		}
+	}
+	return ipsToAdd
+}
+
+func getIPsToRemove(asgIPs map[string]string, sgIPs map[string]string) (ipsToRemove []string) {
+	for i, _ := range sgIPs {
+		if _, ok := asgIPs[i]; !ok {
+			ipsToRemove = append(ipsToRemove, i)
+		}
+	}
+	return ipsToRemove
+}
+
+func getSGIPs(sgID string, ec2Svc *ec2.EC2) (map[string]string, error) {
+	sgIPs := make(map[string]string)
+	sgResponse, err := ec2Svc.DescribeSecurityGroups(&ec2.DescribeSecurityGroupsInput{
+		GroupIds: []*string{
+			aws.String(sgID),
+		},
+	})
+	if err != nil {
+		return sgIPs, err
+	}
+
+	if len(sgResponse.SecurityGroups[0].IpPermissions) != 0 {
+		for _, ipRange := range sgResponse.SecurityGroups[0].IpPermissions[0].IpRanges {
+			sgIPs[aws.StringValue(ipRange.CidrIp)] = aws.StringValue(ipRange.CidrIp)
+		}
+	}
+	return sgIPs, err
+}
+
+func getASGPublicIPs(event Request, asgName string, autoscalingSvc *autoscaling.AutoScaling, ec2Svc *ec2.EC2) (map[string]string, error) {
 	ips := make(map[string]string)
 	asgResponse, err := autoscalingSvc.DescribeAutoScalingGroups(&autoscaling.DescribeAutoScalingGroupsInput{
 		AutoScalingGroupNames: []*string{aws.String(asgName)},
 	})
 	if err != nil {
-		logger.Error("Failed to describe autoscaling groups", zap.Error(err))
 		return ips, err
 	}
 	if asgResponse.String() == "{\n\n}" {
-		err = errors.New("autoscaling group response is empty")
-		logger.Error("Error", zap.Error(err))
-		return ips, err
+		return ips, errors.New("autoscaling group response is empty")
 	}
 	if len(asgResponse.AutoScalingGroups[0].Instances) == 0 {
 		return ips, err
 	}
 
-	var instanceIds []*string
 	for _, instance := range asgResponse.AutoScalingGroups[0].Instances {
-		instanceIds = append(instanceIds, instance.InstanceId)
-	}
-
-	ec2Response, err := ec2Svc.DescribeInstances(&ec2.DescribeInstancesInput{
-		InstanceIds: instanceIds,
-	})
-	if err != nil {
-		logger.Error("Failed to describe EC2 instances", zap.Error(err))
-		return ips, err
-	}
-	for _, reservation := range ec2Response.Reservations {
-		if *reservation.Instances[0].State.Name != "shutting-down" && *reservation.Instances[0].State.Name != "terminated" {
-			ips[aws.StringValue(reservation.Instances[0].PublicIpAddress)+"/32"] = aws.StringValue(reservation.Instances[0].PublicIpAddress)
+		ec2Response, err := ec2Svc.DescribeInstances(&ec2.DescribeInstancesInput{
+			InstanceIds: []*string{instance.InstanceId},
+		})
+		if err != nil {
+			return ips, err
+		}
+		for _, reservation := range ec2Response.Reservations {
+			if *reservation.Instances[0].State.Name != "shutting-down" && *reservation.Instances[0].State.Name != "terminated" {
+				ips[aws.StringValue(reservation.Instances[0].PublicIpAddress)+"/32"] = aws.StringValue(reservation.Instances[0].PublicIpAddress)
+			}
 		}
 	}
-
 	return ips, err
 }
 
